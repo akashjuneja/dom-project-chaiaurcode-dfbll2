d648816757587fe6be4637e1f95b3a93c854cd50
akashjuneja <akashjuneja029@gmail.com>
2023-08-26 23:16:51 +0530

    keyboard codde done

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,22 @@
 console.log('Project 5');
+let x=document.getElementById("insert")
+
+window.addEventListener('keydown',(e)=>{
+  x.innerHTML=`
+  <div class='color'>
+  <table>
+  <tr>
+    <th>key</th>
+    <th>key code</th>
+    <th>Digit</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+</table>
+</div>
+`
+})
